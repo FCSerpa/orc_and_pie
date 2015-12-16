c95ca2b0e103a23980596eebd1edc74b40f6d79d
Francis Serpa <fcserpa@gmail.com>
2015-12-15 19:16:40 -0800

    all first buttons work

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var roll1 = 20;
 var roll2 = 20;
 var damage = 1;
 var result = 'hits';
+var pie;
 
 function initialize(){
 	btnPress1();
@@ -87,6 +88,27 @@ function toHitOrc(){
 	}
 };
 
+function toHitOrcAdv(){
+	toHitOrc();
+	roll2 = d(20);
+	if (roll2 > roll1){
+		roll1 = roll2;
+	}
+	if (roll1 + 5 >= 13){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function toHitOrcDis(){
+	toHitOrc();
+	roll2 = d(20);
+	if (roll2 < roll1){
+		roll1 = roll2;
+	}
+}
+
 function toHitPC(){
 	roll1 = d(20);
 	if (roll1 + 5 >= character.ac){
@@ -97,40 +119,119 @@ function toHitPC(){
 };
 
 function weaponDamage(){
-	return (d(character.weapon) + 3);
+	damage = (d(character.weapon) + 3);
+	return damage;
 };
 
 function backStab(){
 	if (d(20) + 5 >= 10){
-		if (toHitOrc() || toHitOrc()){
-			orcHP -= weaponDamage() + d(6);
+		$('#pTxt').html('You succeed at sneaking up on the orc, ');
+		if (toHitOrcAdv()){
+			damage = weaponDamage() + d(6);
+			orcHP -= damage;
+			$('#pTxt').append('and you roll a ' + roll1 + ' and hit the orc for ' + damage + ' damage. ');
+		} else {
+			$('#pTxt').append('but you roll a ' + roll1 + ' and miss the orc. ');
+		}
+		if (orcHP <= 0){
+			$('#pTxt').append('You have defeated the orc.  You can now help yourself to the pie.  I hope you like ' + flavor() + '.<br>' + youAre());
+			$('div#btnDiv').empty();
+		} else {
+			$('#pTxt').append('The orc is still alive. It looks really mad.  It swings a greataxe at your face.');
+			if (attackPC()){
+				$('#pTxt').append('<br>The orc rolls a ' + roll1 + ' and hits you for ' + damage + ' damage! You have ' + character.hp + ' hit points left.');
+			} else {
+				$('#pTxt').append('<br>The orc rolls a ' + roll1 + ' and misses you.');
+			}
+			if (isDead()){
+				$('#pTxt').append('<br>You have perished.  The orc enjoys its ' + flavor() + ' pie over your mutilated corpse.');
+				$('div#btnDiv').empty();
+			} else {
+				$('div#btnDiv').empty();
+				$('div#btnDiv').append('<button id="attack">Stab at the orc again!</button id="run"><button>Run away!</button>');
+			}
 		}
 	} else {
-		characterWeaponAttack();
+		$('#pTxt').html('You fail at sneaking up on the orc, but since you\'re already right behind it, you try to stab it anyway. <br>');
+		(characterWeaponAttack())
 	}
 };
 
 function characterWeaponAttack(){
 	if (toHitOrc()) {
 		orcHP -= weaponDamage();
+		$('#pTxt').html('You roll a ' + roll1 + ' and hit the orc for ' + damage + ' damage. ');
+	} else {
+		$('#pTxt').append('You roll a ' + roll1 + ' and miss the orc. ');
+	}
+	if (orcHP <= 0){
+		$('#pTxt').append('You have defeated the orc.  You can now help yourself to the pie.  I hope you like ' + flavor() + '.<br>' + youAre());
+		$('div#btnDiv').empty();
+	} else {
+		$('#pTxt').append('The orc is still alive. It looks really mad.  It swings a greataxe at your face.');
+		if (attackPC()){
+			$('#pTxt').append('<br>The orc rolls a ' + roll1 + ' and hits you for ' + damage + ' damage! You have ' + character.hp + ' hit points left.');
+		} else {
+			$('#pTxt').append('<br>The orc rolls a ' + roll1 + ' and misses you.');
+		}
+		if (isDead()){
+			$('#pTxt').append('<br>You have perished.  The orc enjoys its ' + flavor() + ' pie over your mutilated corpse.');
+			$('div#btnDiv').empty();
+		} else {
+			$('div#btnDiv').empty();
+			$('div#btnDiv').append('<button id="attack">Attack the orc again!</button id="run"><button>Run away!</button>');
+		}
 	}
 };
 
 function magicMissile(){
-	orcHP -= d(4) + d(4) + d(4) + 3;
+	damage = d(4) + d(4) + d(4) + 3;
+	orcHP -= damage;
+	$('#pTxt').html('Three magic missiles fly at the orc and hit the it for ' + damage + ' damage. ');
+	if (orcHP <= 0){
+		$('#pTxt').append('You have defeated the orc.  You can now help yourself to the pie.  I hope you like ' + flavor() + '.<br>');
+		$('div#btnDiv').empty();
+	} else {
+		$('#pTxt').append('The orc is injured, but alive. It looks really mad.  It swings a greataxe at your face.');
+		if (attackPC()){
+			$('#pTxt').append('<br>The orc rolls a ' + roll1 + ' and hits you for ' + damage + ' damage! You have ' + character.hp + ' hit points left.');
+		} else {
+			$('#pTxt').append('<br>The orc rolls a ' + roll1 + ' and misses you.');
+		}
+		if (isDead()){
+			$('#pTxt').append('<br>You have perished.  The orc enjoys its ' + flavor() + ' pie over your mutilated corpse.');
+			$('div#btnDiv').empty();
+		} else {
+			$('div#btnDiv').empty();
+			$('div#btnDiv').append('<button id="attack">Try to electrocute the orc with a shocking grasp spell.</button id="run"><button>Run away!</button>');
+		}
+	}
 };
 
 function charm(){
 	if(d(20) + 5 >= 20){
-		return true;
+		$('#pTxt').html('Your kind words somehow charm the orc. Now you will both have pie!  I hope you like ' + flavor() + '.<br>' + youAre());
+		$('div#btnDiv').empty();
 	} else {
-		return false;
+		$('#pTxt').text('You fail to sweet talk the orc.  First it looks comfused, then it looks really mad, then it swings a greataxe at your face.');
+		if (attackPC()){
+			$('#pTxt').append('<br>The orc rolls a ' + roll1 + ' and hits you for ' + damage + ' damage! You have ' + character.hp + ' hit points left.');
+		} else {
+			$('#pTxt').append('<br>The orc rolls a ' + roll1 + ' and misses you.');
+		}
+		if (isDead()){
+			$('#pTxt').append('<br>You have perished.  The orc enjoys its ' + flavor() + ' pie over your mutilated corpse.');
+			$('div#btnDiv').empty();
+		} else {
+			$('div#btnDiv').empty();
+			$('div#btnDiv').append('<button id="attack">Draw a sword and stab the orc.</button id="run"><button>Run away!</button>');
+		}
 	}
 };
 
 function steal(){
 	if((d(20) + 5 >= 10) && (d(20) + 5 >= 15)){
-		$('#pTxt').html('You successfully sneak up behind the orc and steal the pie out from under his nose!  I hope you like ' + flavor() + '.<br>You are a <strong>Rogue</strong>! You\'re all sneaky and stuff.');
+		$('#pTxt').html('You successfully sneak up behind the orc and steal the pie out from under his nose!  I hope you like ' + flavor() + '.<br>' + youAre());
 		$('div#btnDiv').empty();
 		//return true;
 	} else {
@@ -141,7 +242,7 @@ function steal(){
 			$('#pTxt').append('<br>The orc rolls a ' + roll1 + ' and misses you.');
 		}
 		if (isDead()){
-			$('#pTxt').append('<br>You have perished.  The orc enjoys its pie over your mutilated corpse.');
+			$('#pTxt').append('<br>You have perished.  The orc enjoys its ' + flavor() + ' pie over your mutilated corpse.');
 			$('div#btnDiv').empty();
 		} else {
 			$('div#btnDiv').empty();
@@ -166,15 +267,20 @@ function flavor(){
 	if (pieRoll === 1){
 		return 'pumpkin';
 	} else if (pieRoll === 2){
-		return 'apple';
+		pie = 'apple';
+		return pie;
 	} else if (pieRoll === 3){
-		return 'blueberry';
+		pie = 'blueberry';
+		return pie;
 	} else if (pieRoll === 4){
-		return 'lemon meringue';
+		pie = 'lemon meringue';
+		return pie;
 	} else if (pieRoll === 5){
-		return 'banana cream';
+		pie = 'banana cream';
+		return pie;
 	} else if (pieRoll === 6){
-		return 'cherry';
+		pie = 'cherry';
+		return pie;
 	}
 }
 
@@ -183,6 +289,24 @@ function isDead(){
 		return true;
 	} else {
 		return false;
+	}
+}
+
+function youAre(){
+	if (character.characterClass === 'ass') {
+		return 'You are an <strong>Assassin</strong>! You defeat your enemies with stealth and treachery.';
+	} else if (character.characterClass === 'rog') {
+		return 'You are a <strong>Rogue</strong>! You\'re all sneaky and stuff.';
+	} else if (character.characterClass === 'wiz') {
+		return 'You are a <strong>Wizard</strong>! You are a master of the arcane arts.';
+	} else if (character.characterClass === 'brd') {
+		return 'You are a <strong>Bard</strong>! You try to solve your problems with your wits and force of personality.';
+	} else if (character.characterClass === 'brb') {
+		return 'You are a <strong>Barbarian</strong>! You defeat your foes with primal fury.';
+	} else if (character.characterClass === 'ftr') {
+		return 'You are a <strong>Bard</strong>! You defeat your foes with your strength and tactics.';
+	} else if (character.characterClass === 'rng') {
+		return 'You are a <strong>Bard</strong>! You defeat your favored enemies with cunning and skill.';
 	}
 }
 
